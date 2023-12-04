e23b6b8a74d89f42c0801722bc09e8bbce1afbc4
testsajee <152563515+testsajee@users.noreply.github.com>
2023-12-05 04:06:34 +0530

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -61,7 +61,6 @@ def login():
     dynamodb = AWS_SESSION.client('dynamodb')
 
     username = request.args["username"]
-    password = request.args["password"]
 
     dynamodb.scan(
         FilterExpression= "username = " + username + " and password = " + password,
